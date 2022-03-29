48a04eb46ca9f23ae40b4e743d0a26e37071d619
jpcp0614 <jpcp0614@gmail.com>
2022-03-29 02:00:16 -0300

    app.use para users

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import 'express-async-errors';
 import { middlewareError } from './middlewares';
-import productsRoutes from './routes/productsRoutes';
+import { productsRoutes, usersRoutes } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/products', productsRoutes);
+
+app.use('/users', usersRoutes);
 
 app.use(middlewareError);
 
