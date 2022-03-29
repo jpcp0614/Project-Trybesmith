6f0629f45d2d43f3e379cacf4c18bd6a73870e50
jpcp0614 <jpcp0614@gmail.com>
2022-03-29 00:15:27 -0300

    Acrescenta o status

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,12 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { IError } from '../interfaces/index.interfaces';
 
-const middlewareError = (err: Error, _req: Request, res: Response, next: NextFunction) => {
-  const { message, name } = err;
+const middlewareError = (err: IError, _req: Request, res: Response, next: NextFunction) => {
+  const { message, name, status } = err;
 
-  switch (name) {
+  switch (name || status) {
     case 'NotFoundError':
       res.status(StatusCodes.NOT_FOUND).json({ error: message });
+      break;
+    case 400:
+      res.status(StatusCodes.BAD_REQUEST).json({ error: message });
+      break;
+    case 422:
+      res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({ error: message });
       break;
     default:
       console.log(err);
