77c4437138049d32fad4e45ce11477414504bab0
jpcp0614 <jpcp0614@gmail.com>
2022-03-29 00:14:13 -0300

    interface - IError

--- src/interfaces/index.interfaces.ts
+++ src/interfaces/index.interfaces.ts
@@ -1,11 +1,12 @@
 import {
   IProductsList,
   IProductCreate,
-  IProductCreated,
-} from './products.interface';
+  IProductCreated } from './products.interface';
+import { IError } from './error.interface';
 
 export {
   IProductsList,
   IProductCreate,
   IProductCreated,
+  IError,
 };
