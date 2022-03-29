e9f95bca7b11209021d98eb15ca6157751867b8c
jpcp0614 <jpcp0614@gmail.com>
2022-03-29 15:18:12 -0300

    Validação - products

---
+++ src/schemas/ordersSchema.ts
@@ -0,0 +1,13 @@
+import Joi from 'joi';
+
+const orderSchema = Joi.object({
+  products: Joi.array().items(Joi.number())
+    .required().min(1)
+    .messages({
+      'array.base': '422|Products must be an array of numbers',
+      'any.required': '400|Products is required',
+      'array.min': '422|Products can\'t be empty',
+    }),
+});
+
+export default orderSchema;
