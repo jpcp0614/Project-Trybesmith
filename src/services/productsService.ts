ca1194575407a43e7555bdfad9d53f44d70d2fab
jpcp0614 <jpcp0614@gmail.com>
2022-03-29 12:38:32 -0300

    Remove condicional

--- src/services/productsService.ts
+++ src/services/productsService.ts
@@ -1,4 +1,3 @@
-import { NotFoundError } from 'restify-errors';
 import { connection, ProductsModel } from '../models';
 import { IProductsList, IProductCreate, IProductCreated } from '../interfaces';
 
@@ -11,10 +10,6 @@ export default class ProductsService {
 
   public getAll = async (): Promise<IProductsList[]> => {
     const products = await this.model.getAll();
-
-    if (products.length === 0) {
-      throw new NotFoundError('No products found');
-    }
 
     return products;
   };
