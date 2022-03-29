a4329908adb0b53c9bb641e674ace7fd5107f3b9
jpcp0614 <jpcp0614@gmail.com>
2022-03-29 11:28:18 -0300

    Acrescenta o app.use de orders

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import 'express-async-errors';
 import { middlewareError } from './middlewares';
-import { productsRoutes, usersRoutes } from './routes';
+import {
+  productsRoutes,
+  usersRoutes,
+  ordersRoutes } from './routes';
 
 const app = express();
 
@@ -10,6 +13,8 @@ app.use(express.json());
 app.use('/products', productsRoutes);
 
 app.use('/users', usersRoutes);
+
+app.use('/orders', ordersRoutes);
 
 app.use(middlewareError);
 
