2ba23dcb8de054c37c43d0495a231b774551649a
jpcp0614 <jpcp0614@gmail.com>
2022-03-29 01:58:14 -0300

    Validação de username, classe, level e password

---
+++ src/schemas/usersSchema.ts
@@ -0,0 +1,28 @@
+import Joi from 'joi';
+
+const userSchema = Joi.object({
+  username: Joi.string().required().min(3).messages({
+    'string.base': '422|Username must be a string',
+    'any.required': '400|Username is required',
+    'string.min': '422|Username must be longer than 2 characters',
+  }),
+  classe: Joi.string().required().min(3).messages({
+    'string.base': '422|Classe must be a string',
+    'any.required': '400|Classe is required',
+    'string.min': '422|Classe must be longer than 2 characters',
+  }),
+  level: Joi.number().integer().strict().required()
+    .min(1)
+    .messages({
+      'number.base': '422|Level must be a number',
+      'any.required': '400|Level is required',
+      'number.min': '422|Level must be greater than 0',
+    }),
+  password: Joi.string().required().min(8).messages({
+    'string.base': '422|Password must be a string',
+    'any.required': '400|Password is required',
+    'string.min': '422|Password must be longer than 7 characters',
+  }),
+});
+
+export default userSchema;
