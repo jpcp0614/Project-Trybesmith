45bc999343309b433d157a02f819c043e158aabc
jpcp0614 <jpcp0614@gmail.com>
2022-03-28 23:30:13 -0300

    app.use de products e middleware de error

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import 'express-async-errors';
+import middlewareError from './middlewares/error.middleware';
+import productsRoutes from './routes/products.routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', productsRoutes);
+
+app.use(middlewareError);
 
 export default app;
