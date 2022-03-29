35f323c55a41f4ceed896ae670f4834dd01f618b
jpcp0614 <jpcp0614@gmail.com>
2022-03-29 18:23:34 -0300

    Acrecenta o strict

--- src/schemas/ordersSchema.ts
+++ src/schemas/ordersSchema.ts
@@ -1,8 +1,8 @@
 import Joi from 'joi';
 
 const orderSchema = Joi.object({
-  products: Joi.array().items(Joi.number())
-    .required().min(1)
+  products: Joi.array().items(Joi.number().strict()).min(1)
+    .required()
     .messages({
       'array.base': '422|Products must be an array of numbers',
       'any.required': '400|Products is required',
