473751b3bc25f12b8890c0409793847cb0ad9302
jpcp0614 <jpcp0614@gmail.com>
2022-03-29 00:16:03 -0300

    Cria a rota post

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/products.controller';
+import { validateProduct } from '../middlewares/index.middleware';
 
 const router = Router();
 const productsController = new ProductsController();
 
 router.get('/', productsController.getAll);
+router.post('/', validateProduct, productsController.create);
 
 export default router;
