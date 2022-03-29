7639ac34ea6a9adf7beb537d307d8db5c009e1e8
jpcp0614 <jpcp0614@gmail.com>
2022-03-29 15:22:48 -0300

    Rota de post para order

--- src/routes/ordersRoutes.ts
+++ src/routes/ordersRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { OrdersController } from '../controllers';
+import { validateOrder, middlewareAuth } from '../middlewares';
 
 const router = Router();
 const ordersController = new OrdersController();
 
+const validAuth = [middlewareAuth, validateOrder];
+
+router.post('/', validAuth, ordersController.create);
 router.get('/', ordersController.getAll);
 
 export default router;
